c1d8a253bd158a718cc0e48004913e3aa0b5b12c
GearKite <onyx-commute-quit@duck.com>
2024-11-12 22:19:56 +0200

    hyprland: add window rules for always open apps

--- home/desktop/hyprland/settings/windowrules.nix
+++ home/desktop/hyprland/settings/windowrules.nix
@@ -1,4 +1,13 @@
 { config, pkgs, ... }:
 {
-  wayland.windowManager.hyprland.settings = { };
+  wayland.windowManager.hyprland.settings = {
+    windowrulev2 = [
+      "workspace 11, class:(thunderbird)"
+
+      "workspace 15, class:(Element)"
+      "workspace 16, class:(nuclear)"
+      "workspace 17, class:(Element)"
+      "workspace 18, class:(Signal-desktop)"
+    ];
+   };
 }
