16b461c01ebc53c703615efeea3ebccc1e3b9b5f
GearKite <onyx-commute-quit@duck.com>
2024-11-16 08:35:28 +0200

    hyprland: add more workspaces

--- home/desktop/hyprland/settings/workspaces.nix
+++ home/desktop/hyprland/settings/workspaces.nix
@@ -1,14 +1,23 @@
 { config, pkgs, ... }:
 {
   wayland.windowManager.hyprland.settings = {
-    workspace = [
-      "1, monitor:desc:GIGA-BYTE TECHNOLOGY CO. LTD. G27Q 21502B003480, default:true"
-      "2, monitor:desc:GIGA-BYTE TECHNOLOGY CO. LTD. G27Q 21502B003480"
-      "3, monitor:desc:GIGA-BYTE TECHNOLOGY CO. LTD. G27Q 21502B003480"
+    "$monitor1" = "desc:GIGA-BYTE TECHNOLOGY CO. LTD. G27Q 21502B003480";
+    "$monitor2" = "desc:LG Electronics L194WT 0x000627AF";
 
-      "4, monitor:desc:LG Electronics L194WT 0x000627AF, default:true"
-      "5, monitor:desc:LG Electronics L194WT 0x000627AF"
-      "6, monitor:desc:LG Electronics L194WT 0x000627AF"
+    workspace = [
+      "1, monitor:$monitor1, default:true"
+      "2, monitor:$monitor1"
+      "3, monitor:$monitor1"
+
+      "11, monitor:$monitor1"
+      "12, monitor:$monitor1"
+      "13, monitor:$monitor1"
+      "14, monitor:$monitor1"
+
+      "15, monitor:$monitor2, default:true"
+      "16, monitor:$monitor2"
+      "17, monitor:$monitor2"
+      "18, monitor:$monitor2"
     ];
   };
 }
