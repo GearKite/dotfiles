da10007daa51bda95bbaa7e51877c5833371f764
GearKite <onyx-commute-quit@duck.com>
2024-12-19 16:57:35 +0200

    Add flatpak

--- system/desktop.nix
+++ system/desktop.nix
@@ -47,4 +47,13 @@
   hardware.i2c = {
     enable = true;
   };
+
+  services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
 }
