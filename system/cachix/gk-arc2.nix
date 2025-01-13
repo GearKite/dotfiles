ecc61a3e975fae497e9938265818704a09a8c132
GearKite <onyx-commute-quit@duck.com>
2025-01-13 12:27:13 +0200

    cachix: add gk-arc2

---
+++ system/cachix/gk-arc2.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://gk-arc2.cachix.org"
+      ];
+      trusted-public-keys = [
+        "gk-arc2.cachix.org-1:iJOofh4wNI6QkwKv/Js7QvMPhHrtW6/HyjQw6+uugJM="
+      ];
+    };
+  };
+}
