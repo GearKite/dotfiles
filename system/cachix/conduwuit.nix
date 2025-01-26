2b7e4a42c65c1e86b9eaed385a3f0a4473bd3440
GearKite <onyx-commute-quit@duck.com>
2025-01-26 23:38:16 +0200

    add conduwuit binary caches

---
+++ system/cachix/conduwuit.nix
@@ -0,0 +1,17 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://attic.kennel.juneis.dog/conduwuit"
+        "https://attic.kennel.juneis.dog/conduit"
+        "https://conduwuit.cachix.org"
+      ];
+      trusted-public-keys = [
+        "conduwuit:BbycGUgTISsltcmH0qNjFR9dbrQNYgdIAcmViSGoVTE="
+        "conduit:eEKoUwlQGDdYmAI/Q/0slVlegqh/QmAvQd7HBSm21Wk="
+        "conduwuit.cachix.org-1:MFRm6jcnfTf0jSAbmvLfhO3KBMt4px+1xaereWXp8Xg="
+      ];
+    };
+  };
+}
