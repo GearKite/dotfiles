24a65286abcd0daf733782e50c3e57c62ccecf8b
GearKite <onyx-commute-quit@duck.com>
2025-06-17 00:11:37 +0300

    services.pulseaudio -> services.pulseaudio

--- modules/host/audio/default.nix
+++ modules/host/audio/default.nix
@@ -1,6 +1,6 @@
 _: {
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
