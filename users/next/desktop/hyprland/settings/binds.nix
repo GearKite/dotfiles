36c26c6acde3f97cd85034ca687e0761c98cfab7
GearKite <onyx-commute-quit@duck.com>
2025-08-06 18:10:06 +0300

    nixfmt

--- users/next/desktop/hyprland/settings/binds.nix
+++ users/next/desktop/hyprland/settings/binds.nix
@@ -7,80 +7,79 @@
   wayland.windowManager.hyprland.settings = {
     "$mainMod" = "SUPER";
 
-    bind =
-      [
-        # Actions
-        "$mainMod, RETURN, exec, ${config.modules.executable.terminal}" # Open Alactritty
-        "$mainMod, Q, killactive" # Close current window
-        "$mainMod, M, exit" # Exit Hyprland
-        "$mainMod, E, exec, ${config.modules.executable.filemanager}" # Opens the filemanager
-        "$mainMod, T, togglefloating" # Toggle between tiling and floating window
-        "$mainMod, F, fullscreen" # Open the window in fullscreen
-        "$mainMod CTRL, RETURN, exec, fuzzel" # Open fuzzel
-        "$mainMod, P, pseudo," # dwindle
-        "$mainMod, J, togglesplit," # dwindle
-        "$mainMod, B, exec, ${config.modules.executable.browser}" # Opens the browser
-        "$mainMod, V, exec, cliphist list | fuzzel -d | cliphist decode | wl-copy"
-        "$mainMod, L, exec, loginctl lock-session" # lock
-        "$mainMod CTRL, L, exec, sudo halt -f" # force halt
+    bind = [
+      # Actions
+      "$mainMod, RETURN, exec, ${config.modules.executable.terminal}" # Open Alactritty
+      "$mainMod, Q, killactive" # Close current window
+      "$mainMod, M, exit" # Exit Hyprland
+      "$mainMod, E, exec, ${config.modules.executable.filemanager}" # Opens the filemanager
+      "$mainMod, T, togglefloating" # Toggle between tiling and floating window
+      "$mainMod, F, fullscreen" # Open the window in fullscreen
+      "$mainMod CTRL, RETURN, exec, fuzzel" # Open fuzzel
+      "$mainMod, P, pseudo," # dwindle
+      "$mainMod, J, togglesplit," # dwindle
+      "$mainMod, B, exec, ${config.modules.executable.browser}" # Opens the browser
+      "$mainMod, V, exec, cliphist list | fuzzel -d | cliphist decode | wl-copy"
+      "$mainMod, L, exec, loginctl lock-session" # lock
+      "$mainMod CTRL, L, exec, sudo halt -f" # force halt
 
-        # Screenshots
-        ", Print, exec, hyprshot -c -m output -o ~/Pictures/Screenshots -f $(date '+%Y-%m-%dT%H-%M-%S').png" # monitor
-        "CTRL, Print, exec, hyprshot -c --raw -m window -- echo | satty --filename - --fullscreen --output-filename ~/Pictures/Screenshots/$(date '+%Y-%m-%dT%H-%M-%S')-satty.png" # window + annotation
-        "SHIFT, Print, exec, hyprshot --raw -m region -- echo | satty --filename - --fullscreen --output-filename ~/Pictures/Screenshots/$(date '+%Y-%m-%dT%H-%M-%S')-satty.png" # region + annotation
+      # Screenshots
+      ", Print, exec, hyprshot -c -m output -o ~/Pictures/Screenshots -f $(date '+%Y-%m-%dT%H-%M-%S').png" # monitor
+      "CTRL, Print, exec, hyprshot -c --raw -m window -- echo | satty --filename - --fullscreen --output-filename ~/Pictures/Screenshots/$(date '+%Y-%m-%dT%H-%M-%S')-satty.png" # window + annotation
+      "SHIFT, Print, exec, hyprshot --raw -m region -- echo | satty --filename - --fullscreen --output-filename ~/Pictures/Screenshots/$(date '+%Y-%m-%dT%H-%M-%S')-satty.png" # region + annotation
 
-        # Move focus with mainMod + arrow keys
-        "$mainMod, left, movefocus, l" # Move focus left
-        "$mainMod, right, movefocus, r" # Move focus right
-        "$mainMod, up, movefocus, u" # Move focus up
-        "$mainMod, down, movefocus, d" # Move focus down
+      # Move focus with mainMod + arrow keys
+      "$mainMod, left, movefocus, l" # Move focus left
+      "$mainMod, right, movefocus, r" # Move focus right
+      "$mainMod, up, movefocus, u" # Move focus up
+      "$mainMod, down, movefocus, d" # Move focus down
 
-        # Move windows with arrow keys
-        "$mainMod SHIFT, left, movewindow, l"
-        "$mainMod SHIFT, right, movewindow, r"
-        "$mainMod SHIFT, up, movewindow, u"
-        "$mainMod SHIFT, down, movewindow, d"
+      # Move windows with arrow keys
+      "$mainMod SHIFT, left, movewindow, l"
+      "$mainMod SHIFT, right, movewindow, r"
+      "$mainMod SHIFT, up, movewindow, u"
+      "$mainMod SHIFT, down, movewindow, d"
 
-        # Scroll through existing workspaces with mainMod + scroll
-        "$mainMod, mouse_down, workspace, e+1" # Scroll workspaces
-        "$mainMod, mouse_up, workspace, e-1" # Scroll workspaces
+      # Scroll through existing workspaces with mainMod + scroll
+      "$mainMod, mouse_down, workspace, e+1" # Scroll workspaces
+      "$mainMod, mouse_up, workspace, e-1" # Scroll workspaces
 
-        # workspaces
-        "$mainMod, 0, workspace, 10"
-        "$mainMod SHIFT, 0, movetoworkspace, 10"
-      ]
-      ++ (
-        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-        builtins.concatLists (
-          builtins.genList (
-            i:
-            let
-              ws = i + 1;
-            in
-            [
-              "$mainMod, code:1${toString i}, workspace, ${toString ws}"
-              "$mainMod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-            ]
-          ) 9
-        )
+      # workspaces
+      "$mainMod, 0, workspace, 10"
+      "$mainMod SHIFT, 0, movetoworkspace, 10"
+    ]
+    ++ (
+      # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+      builtins.concatLists (
+        builtins.genList (
+          i:
+          let
+            ws = i + 1;
+          in
+          [
+            "$mainMod, code:1${toString i}, workspace, ${toString ws}"
+            "$mainMod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+          ]
+        ) 9
       )
+    )
 
-      ++ (
-        # workspaces with F keys
-        # binds $mod + [shift +] F{1..8} to [move to] workspace {11..18}
-        builtins.concatLists (
-          builtins.genList (
-            i:
-            let
-              ws = i + 1;
-            in
-            [
-              "$mainMod, F${toString ws}, workspace, 1${toString ws}"
-              "$mainMod SHIFT, F${toString ws}, movetoworkspace, 1${toString ws}"
-            ]
-          ) 8
-        )
-      );
+    ++ (
+      # workspaces with F keys
+      # binds $mod + [shift +] F{1..8} to [move to] workspace {11..18}
+      builtins.concatLists (
+        builtins.genList (
+          i:
+          let
+            ws = i + 1;
+          in
+          [
+            "$mainMod, F${toString ws}, workspace, 1${toString ws}"
+            "$mainMod SHIFT, F${toString ws}, movetoworkspace, 1${toString ws}"
+          ]
+        ) 8
+      )
+    );
 
     bindm = [
       # Move/resize windows with mainMod + LMB/RMB and dragging
