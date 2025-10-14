39675f8db8c9cb63cd2fa62a39675d79f52297d4
GearKite <onyx-commute-quit@duck.com>
2025-10-14 14:23:15 +0300

    update locales

--- modules/host/locale/default.nix
+++ modules/host/locale/default.nix
@@ -1,6 +1,9 @@
 { lib, ... }:
 {
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "lv_LV.UTF-8";
+  i18n.extraLocales = [
+    "en_GB.UTF-8/UTF-8"
+  ];
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "lv_LV.UTF-8";
@@ -11,7 +14,7 @@
     LC_NUMERIC = "lv_LV.UTF-8";
     LC_PAPER = "lv_LV.UTF-8";
     LC_TELEPHONE = "lv_LV.UTF-8";
-    LC_TIME = "lv_LV.UTF-8";
+    LC_TIME = "en_DK.UTF-8";
   };
 
   time.timeZone = lib.mkDefault "Europe/Riga";
