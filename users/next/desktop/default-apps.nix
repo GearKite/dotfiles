d9b087b580481b5a5e66dc88b5a44ed6e0f2547b
GearKite <onyx-commute-quit@duck.com>
2025-10-20 16:32:38 +0300

    fix 'mimeapps.list would be clobbered'

--- users/next/desktop/default-apps.nix
+++ users/next/desktop/default-apps.nix
@@ -1,14 +1,18 @@
 _: {
   home-manager.users.next = {
-    xdg.mimeApps = {
-      enable = true;
-      defaultApplications = {
-        "image/jpeg" = "oculante.desktop";
-        "image/png" = "oculante.desktop";
-        "image/gif" = "oculante.desktop";
-        "image/webp" = "oculante.desktop";
-        "image/tiff" = "oculante.desktop";
+    xdg = {
+      mimeApps = {
+        enable = true;
+        defaultApplications = {
+          "image/jpeg" = "oculante.desktop";
+          "image/png" = "oculante.desktop";
+          "image/gif" = "oculante.desktop";
+          "image/webp" = "oculante.desktop";
+          "image/tiff" = "oculante.desktop";
+        };
       };
+
+      configFile."mimeapps.list".force = true;
     };
   };
 }
