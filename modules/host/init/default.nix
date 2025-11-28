f5ebab7c605e846aea281143c5af92fde5197155
GearKite <onyx-commute-quit@duck.com>
2025-11-28 15:48:08 +0200

    Use Linux kernel from stable

--- modules/host/init/default.nix
+++ modules/host/init/default.nix
@@ -1,6 +1,9 @@
-_: {
+{ stable, ... }:
+{
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   services.fwupd.enable = true;
+
+  boot.kernelPackages = stable.pkgs.linuxPackages;
 }
