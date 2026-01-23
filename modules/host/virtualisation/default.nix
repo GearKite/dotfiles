ac7d1fccddb44a141c5ff7bc27eba40e6b2682f3
GearKite <onyx-commute-quit@duck.com>
2026-01-23 10:32:54 +0200

    add qemu uefi boot script

--- modules/host/virtualisation/default.nix
+++ modules/host/virtualisation/default.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = [ pkgs.qemu ];
+  environment.systemPackages = [
+    pkgs.qemu
+    (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
+      qemu-system-x86_64 \
+        -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
+        "$@"
+    '')
+  ];
   programs.virt-manager.enable = true;
 
   virtualisation = {
